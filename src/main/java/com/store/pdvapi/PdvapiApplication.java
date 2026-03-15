c2b6cf621906b376f18cfe6bc8c1a49a1c4fbb23
lucas-luch <luchlucas2@gmail.com>
2026-03-15 17:50:28 -0300

    chore: bootstrap do projeto com Spring Boot

---
+++ src/main/java/com/store/pdvapi/PdvapiApplication.java
@@ -0,0 +1,13 @@
+package com.store.pdvapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvapiApplication.class, args);
+	}
+
+}
