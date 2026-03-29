c40794abcde250aeef5d5741dd5bc1103d27696c
lucas-luch <luchlucas2@gmail.com>
2026-03-29 12:57:04 -0300

    feat: add ItemPedido entity

---
+++ src/main/java/com/store/pdvapi/model/ItemPedido.java
@@ -0,0 +1,70 @@
+package com.store.pdvapi.model;
+
+public class ItemPedido {
+    private Long id;
+    private Pedido pedido;
+    private Produto produto;
+    private int quantidade;
+    private double precoUnitario;
+    private double subtotal;
+
+    public ItemPedido() {
+    }
+
+    public ItemPedido(Long id, Pedido pedido, Produto produto, int quantidade, double precoUnitario, double subtotal) {
+        this.id = id;
+        this.pedido = pedido;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+        this.subtotal = subtotal;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+}
